1559cb55e3f892fb4bb83e8a3a6b14f4dc5a1fbb
Vladislav Gushin <vdevapple@icloud.com>
2018-08-28 09:55:27 +0300

    add new templates

---
+++ MyWork/ManagingStateObjects/PowerOfResult .swift
@@ -0,0 +1,71 @@
+//
+//  PowerOfResult .swift
+//  MyWork
+//
+//  Created by Vladislav Gushin on 28/08/2018.
+//  Copyright © 2018 Vladislav Gushin. All rights reserved.
+//
+
+import UIKit
+
+//MARK: - Simple result
+enum PowerOfResult<Value> {
+    case success(Value)
+    case failure(Error)
+}
+
+//MARK: - Typed errors
+enum TypedPowerOfResult<Value, Error: Swift.Error> {
+    case success(Value)
+    case failure(Error)
+}
+
+//MARK: Throwing
+extension TypedPowerOfResult {
+    func resolve() throws -> Value {
+        switch self {
+        case .success(let value):
+            return value
+        case .failure(let error):
+            throw error
+        }
+    }
+    
+    /*
+     var result: Result<[SearchResult], SearchResultsLoader.Error>?
+     loader.loadResults(matching: "query") {
+     result = $0
+     }
+     let searchResults = try result?.resolve()
+    */
+}
+
+//MARK: - Decoding
+extension TypedPowerOfResult where Value == Data {
+    func decoded<T: Decodable>() throws -> T {
+        let decoder = JSONDecoder()
+        let data = try resolve()
+        return try decoder.decode(T.self, from: data)
+    }
+    
+    /*
+     load { [weak self] result in
+     do {
+     let user = try result.decoded() as User
+     self?.userDidLoad(user)
+     } catch {
+     self?.handle(error)
+     }
+     }
+     */
+}
+
+//MARK: - Using
+struct TestErrors {
+    enum LoadingError: Error { }
+    typealias Handler = (TypedPowerOfResult<Data, LoadingError>) -> Void
+
+    func load(then handler: @escaping Handler) {
+        //...
+    }
+}
---
+++ MyWork/ManagingStateObjects/TypeSafeIdentifiers.swift
@@ -0,0 +1,72 @@
+//
+//  TypeSafeIdentifiers.swift
+//  MyWork
+//
+//  Created by Vladislav Gushin on 28/08/2018.
+//  Copyright © 2018 Vladislav Gushin. All rights reserved.
+//
+
+import UIKit
+
+struct Identifier: Hashable {
+    let string: String
+}
+
+extension Identifier: ExpressibleByStringLiteral {
+    init(stringLiteral value: String) {
+        string = value
+    }
+}
+//to make it simple to print an identifier
+extension Identifier: CustomStringConvertible {
+    var description: String {
+        return string
+    }
+}
+
+extension Identifier: Codable {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        string = try container.decode(String.self)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(string)
+    }
+}
+
+//Even more type safety
+
+struct TypeSafeUser {
+    let id: Identifier
+    let name: String
+}
+
+//MARK: Using
+struct TypeIdentifiersTest {
+    func test() {
+        let id = Identifier(string: "new-user")
+        let id2: Identifier = "new-user" // ExpressibleByStringLiteral
+        let user = TypeSafeUser(id: "new-user", name: "John")
+    }
+}
+
+//MARK: - Generalizing our generic
+protocol Identifiable {
+    associatedtype RawIdentifier: Codable
+    var id: Identifier2<Self> { get }
+}
+
+struct Identifier2<Value : Identifiable> {
+    let rawValue: Value.RawIdentifier
+    init(rawValue: Value.RawIdentifier) {
+        self.rawValue = rawValue
+    }
+}
+
+struct GroupTEST: Identifiable {
+    typealias RawIdentifier = Int
+    let id: Identifier2<GroupTEST>
+    let name: String
+}
