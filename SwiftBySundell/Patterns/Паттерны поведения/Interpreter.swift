0d863969bae4fbcc551633a9f740ee8385dd28f2
Vlad Gushchin <v.d.gushin@live.ru>
2019-04-22 09:26:41 +0300

    add net templates

--- SwiftBySundell/Patterns/Паттерны поведения/Interpreter.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Interpreter.swift
-//  SwiftBySundell
-//
-//  Created by Vladislav Gushin on 29/01/2019.
-//  Copyright © 2019 Vladislav Gushin. All rights reserved.
-//
-
-import Foundation
--- SwiftBySundell/Patterns/Паттерны поведения/Стратегия.swift
+++ SwiftBySundell/Patterns/Паттерны поведения/Стратегия.swift
@@ -2,7 +2,7 @@
 //  Стратегия.swift
 //  SwiftBySundell
 //
-//  Created by Vlad Gushchin on 16/04/2019.
+//  Created by Vlad Gushchin on 22/04/2019.
 //  Copyright © 2019 Vladislav Gushin. All rights reserved.
 //
 
@@ -20,42 +20,47 @@ fileprivate class ConcreteStrategy2: Strategy {
     func algorithm() { }
 }
 
-fileprivate class Client {
-    private let strategy: Strategy
+fileprivate class Context {
+    let strategy: Strategy
 
     init(strategy: Strategy) {
         self.strategy = strategy
-        //run algorithm
+    }
+
+    func executeAlgorithm() {
         self.strategy.algorithm()
     }
 }
 
-/*Теперь рассмотрим конкретный пример. Существуют различные легковые машины, которые используют разные источники энергии: электричество, бензин, газ и так далее. Есть гибридные автомобили. В целом они похожи и отличаются преимущественно видом источника энергии. Не говоря уже о том, что мы можем изменить применяемый источник энергии, модифицировав автомобиль. И в данном случае вполне можно применить паттерн стратегию:*/
+//=====================================================================================
 fileprivate protocol Movable {
     func move()
 }
 
+//Бензиновый ход
 fileprivate class PetrolMove: Movable {
     func move() {
-        print("Перемещение на бензине")
+        print("Бензиновый ход")
     }
 }
 
+//Электрический ход
 fileprivate class ElectricMove: Movable {
     func move() {
-        print("Перемещение на электричестве")
+        print("Электрический ход")
     }
 }
 
+//Машина
 fileprivate class Car {
     private let passengers: Int
     private let model: String
     var movable: Movable
 
-    init(num: Int, model: String, mov: Movable) {
-        self.passengers = num
+    init(passengers: Int, model: String, movable: Movable) {
+        self.passengers = passengers
         self.model = model
-        self.movable = mov
+        self.movable = movable
     }
 
     func move() {
@@ -64,9 +69,9 @@ fileprivate class Car {
 }
 
 fileprivate func main() {
-    let auto = Car(num: 4, model: "Volvo", mov: PetrolMove())
+    let auto = Car(passengers: 4, model: "Volvo", movable: PetrolMove())
     auto.move()
-    //в нужных местах мы можем менять поведение класса
     auto.movable = ElectricMove()
     auto.move()
+    print("car")
 }
